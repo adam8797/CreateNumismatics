25513a7517a973e03af47c7bcaf116b554e744ef
eydson <eydsonhilario4@gmail.com>
2026-06-01 16:16:16 -0300

    Moedas e conversões

--- common/src/main/java/dev/ithundxr/createnumismatics/content/backend/Coin.java
+++ common/src/main/java/dev/ithundxr/createnumismatics/content/backend/Coin.java
@@ -25,11 +25,11 @@ import static dev.ithundxr.createnumismatics.registry.NumismaticsIcons.*;
  */
 public enum Coin implements INamedIconOptions {
     SPUR(1, Rarity.COMMON, I_COIN_SPUR, "\uF011"),
-    BEVEL(8, Rarity.COMMON, I_COIN_BEVEL, "\uF012"), // 8 spurs
-    SPROCKET(16, Rarity.COMMON, I_COIN_SPROCKET, "\uF013"), // 16 spurs, 2 bevels
-    COG(64, Rarity.UNCOMMON, I_COIN_COG, "\uF014"), // 64 spurs, 8 bevels, 4 sprockets
-    CROWN(512, Rarity.RARE, I_COIN_CROWN, "\uF015"), // 512 spurs, 64 bevels, 32 sprockets, 8 cogs
-    SUN(4096, Rarity.EPIC, I_COIN_SUN, "\uF016") // 4096 spurs, 512 bevels, 256 sprockets, 64 cogs, 8 crowns
+    BEVEL(50, Rarity.COMMON, I_COIN_BEVEL, "\uF012"), // 50 cobre
+    SPROCKET(1000, Rarity.COMMON, I_COIN_SPROCKET, "\uF013"), // 1.000 cobre, 20 ferro
+    COG(10000, Rarity.UNCOMMON, I_COIN_COG, "\uF014"), // 10.000 cobre, 200 ferro, 10 ouro
+    CROWN(100000, Rarity.RARE, I_COIN_CROWN, "\uF015"), // 100.000 cobre, 2.000 ferro, 100 ouro, 10 diamante
+    SUN(400000, Rarity.EPIC, I_COIN_SUN, "\uF016") // 400.000 cobre, 8.000 ferro, 400 ouro, 40 diamante, 4 jade
     ;
 
     public final int value; // in terms of spurs
